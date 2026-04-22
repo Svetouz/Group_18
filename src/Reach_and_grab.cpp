85d4ee9e85b6311391a736067bc71ef68676fe0f
Svetouz <s.ouzounov@student.fontys.nl>
2026-04-22 15:04:47 +0200

    Overhaul for testing

--- src/Reach_and_grab.cpp
+++ src/Reach_and_grab.cpp
@@ -1,50 +1,50 @@
-#include <Arduino.h>
-#include "Servo.h"
-#include "Reach_and_grab.h"
+// #include <Arduino.h>
+// #include "Servo.h"
+// #include "Reach_and_grab.h"
 
-//Global Variables
+// //Global Variables
 
-int openGrip; //Angle that opens the gripper, placeholder value, 
-int closeGrip; //Angle that closes the gripper, placeholder value
-int startShoulder; //Starting angle for shoulder, placeholder value
-int startElbow; //Starting angle for elbow, placeholder value
-int startWrist; //Starting angle for wrist, placeholder value
+// int openGrip = 0; //Angle that opens the gripper, placeholder value, 
+// int closeGrip; //Angle that closes the gripper, placeholder value
+// int startShoulder =85; //Starting angle for shoulder, placeholder value
+// int startElbow=90; //Starting angle for elbow, placeholder value
+// int startWrist=90; //Starting angle for wrist, placeholder value
 
-// To be reused to store the last written angles, initial values co-
-//rrespond to the starting position of the arm
-ServoAngles lastAngles = {startShoulder, startElbow, startWrist, closeGrip}; 
+// // To be reused to store the last written angles, initial values co-
+// //rrespond to the starting position of the arm
+// ServoAngles lastAngles = {startShoulder, startElbow, startWrist, closeGrip}; 
 
-ServoAngles writeServos(int angle1 = -1, int angle2 = -1, int angle3 = -1,
-                        int angle4 = -1){
-    //Writes angles to the servos, input in degrees. If an angle is -1,
-    //it will not be updated and the last angle will be reused.
+// ServoAngles writeServos(int angle1 = -1, int angle2 = -1, int angle3 = -1,
+//                         int angle4 = -1){
+//     //Writes angles to the servos, input in degrees. If an angle is -1,
+//     //it will not be updated and the last angle will be reused.
 
-  if(angle1 != -1) { shoulder.write(angle1); lastAngles.angle1 = angle1; }
-  delay(250);
-  if(angle2 != -1) { elbow.write(angle2); lastAngles.angle2 = angle2; }
-  delay(250);
-  if(angle3 != -1) { wrist.write(angle3); lastAngles.angle3 = angle3; }
-  delay(250);
-  if(angle4 != -1) { gripper.write(angle4); lastAngles.angle4 = angle4; }
-  delay(250);
+//   if(angle1 != -1) { shoulder.write(angle1); lastAngles.angle1 = angle1; }
+//   delay(250);
+//   if(angle2 != -1) { elbow.write(angle2); lastAngles.angle2 = angle2; }
+//   delay(250);
+//   if(angle3 != -1) { wrist.write(angle3); lastAngles.angle3 = angle3; }
+//   delay(250);
+//   if(angle4 != -1) { gripper.write(angle4); lastAngles.angle4 = angle4; }
+//   delay(250);
   
-  return lastAngles;
-}
+//   return lastAngles;
+// }
 
-void reachAndGrab(){
-  // Opens the gripper and moves the arm into grip position, 
-  // close the gripper, and then moves arm into the carrying position.
-  //(angles are placeholders and need to be adjusted):
+// void reachAndGrab(){
+//   // Opens the gripper and moves the arm into grip position, 
+//   // close the gripper, and then moves arm into the carrying position.
+//   //(angles are placeholders and need to be adjusted):
   
-  //Open gripper
-  ServoAngles openGripper = writeServos(-1, -1, -1,openGrip);
+//   //Open gripper
+//   ServoAngles openGripper = writeServos(-1, -1, -1,openGrip);
 
-  // Move into grip position
-  ServoAngles intoGripPosition = writeServos(45, 90, 135, -1);
+//   // Move into grip position
+//   ServoAngles intoGripPosition = writeServos(45, 90, 135, -1);
   
-  // Close gripper
-  ServoAngles closeGripper = writeServos(45, 90, 135, closeGrip); 
+//   // Close gripper
+//   ServoAngles closeGripper = writeServos(45, 90, 135, closeGrip); 
 
-  // Move back to starting position
-  ServoAngles backToStart = writeServos(90, 90, 90, -1);  
-}
+//   // Move back to starting position
+//   ServoAngles backToStart = writeServos(90, 90, 90, -1);  
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,20 @@
 
 
 // Servos (PWM pins)
-const int SHOULDER_PWM_PIN = 12;
-const int ELBOW_PWM_PIN = 11;
-const int WRIST_PWM_PIN = 10;
-const int GRIPPER_PWM_PIN = 9;
+const int SHOULDER_PWM_PIN = 22; //needs 1.8 amps alone
+const int ELBOW_PWM_PIN = 24;
+const int WRIST_PWM_PIN = 26;
+const int GRIPPER_PWM_PIN = 28;
 
 // Motor Driver
 const int MOTOR_STEP_PIN = 3;
 const int MOTOR_DIR_PIN = 2;
-// const int MOTOR_EN_PIN = 7; // is floating
 
 // Motor Driver correspond to MS pins, should be THREE of them,
 // of which only TWO are being used!! placeholder pin numbers
-const int MOTOR_N1A_PIN = 1;
-const int MOTOR_N1B_PIN = 5;
-const int MOTOR_N2A_PIN = 6;
-const int MOTOR_N2B_PIN = 7;
-
-// ToF Sensor
-const int TOF_SDA_PIN = 20;
-const int TOF_SCL_PIN = 21;
+// const int MS1 = 1;
+// const int MS2= 5;
+// const int MS3 = 6;
 
 // Servo variables
 Servo shoulder;
@@ -35,7 +29,7 @@ Servo gripper;
 
 // Helper functions
 
-// Stepper - Step/Dir mode (use ONE of these, not both)
+// Stepper - Step/Dir mode 
 AccelStepper stepperStepDir(AccelStepper::DRIVER, MOTOR_STEP_PIN, MOTOR_DIR_PIN);
 
 
@@ -49,22 +43,23 @@ void setup() {
     wrist.attach(WRIST_PWM_PIN);
     gripper.attach(GRIPPER_PWM_PIN);
 
-
     stepperStepDir.setMaxSpeed(800);      // steps per second
     stepperStepDir.setAcceleration(500);   // steps per second
 
 }
 
 
-
 void loop(){
-  writeServos(0);
-  //reachAndGrab();
-  // //for finding endopoints of a servo (currently wrist)
+  shoulder.write(85);
+  elbow.write(90);
+  wrist.write(50);
+  //gripper.write(50);
+}
+  
+  //for finding endopoints of a servo (currently wrist)
   // for (int i = 180; i>0 ; i = i -2)
   // {
   //   writeServos(0,0,0,i);    
   //   Serial.println("Angle: " + i);
   //   delay(250);
   // };
-}
