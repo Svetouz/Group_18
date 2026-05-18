97fd8da987eefdd4aa32e1f992fb8786552d5019
Svetouz <s.ouzounov@student.fontys.nl>
2026-05-18 14:09:04 +0200

    Change writeServos to not break Serial Monitor for testing

--- src/Reach_and_grab.cpp
+++ src/Reach_and_grab.cpp
@@ -4,8 +4,8 @@
 
 //Global Variables
 
-int startShoulder = 84; // 0-84 anticlockwise
-int startElbow = 165; // 70-156 clockwise
+int startShoulder = 113; // 0-113 anticlockwise
+int startElbow = 130; // 0-130 clockwise
 int startWrist = 0; //  0-180 anticlockwise
 int openGrip = 110; // 90-178 anticlockwise
 int closeGrip = 177; // 178 pulls more, firmer grip or waste?
@@ -16,80 +16,41 @@ ServoAngles lastAngles = {startShoulder, startElbow, startWrist, closeGrip};
 
 
 ServoAngles writeServos(int angle1 = -1, int angle2 = -1, int angle3 = -1,
-                        int angle4 = -1, int speed = 20){
-    //Writes angles to the servos, input in degrees. If an angle is -1,
-    //it will not be updated and the last angle will be reused.
-    //speed parameter controls smoothness (ms delay between increments)
+                        int angle4 = -1, int speed = 20) {
+    static unsigned long lastUpdate = 0;
+    static int currentAngles[4] = {lastAngles.angle1, lastAngles.angle2, lastAngles.angle3, lastAngles.angle4};
+    static int targetAngles[4] = {lastAngles.angle1, lastAngles.angle2, lastAngles.angle3, lastAngles.angle4};
 
-  if(angle1 != -1) { 
-    int currentAngle = lastAngles.angle1;  // Use tracked value, not servo.read()
-    if (currentAngle < angle1) {
-      for (int angle = currentAngle; angle <= angle1; angle++) {
-        shoulder.write(angle);
-        delay(speed);
-      }
-    } else if (currentAngle > angle1) {
-      for (int angle = currentAngle; angle >= angle1; angle--) {
-        shoulder.write(angle);
-        delay(speed);
-      }
+    // Update target angles if new values are provided
+    if (angle1 != -1) targetAngles[0] = angle1;
+    if (angle2 != -1) targetAngles[1] = angle2;
+    if (angle3 != -1) targetAngles[2] = angle3;
+    if (angle4 != -1) targetAngles[3] = angle4;
+
+    // Check if it's time to update the servo positions
+    if (millis() - lastUpdate >= speed) {
+        lastUpdate = millis();
+
+        // Update each servo incrementally
+        for (int i = 0; i < 4; i++) {
+            if (currentAngles[i] < targetAngles[i]) {
+                currentAngles[i]++;
+            } else if (currentAngles[i] > targetAngles[i]) {
+                currentAngles[i]--;
+            }
+        }
+
+        // Write the updated angles to the servos
+        shoulder.write(currentAngles[0]);
+        elbow.write(currentAngles[1]);
+        wrist.write(currentAngles[2]);
+        gripper.write(currentAngles[3]);
+
+        // Update the lastAngles structure
+        lastAngles = {currentAngles[0], currentAngles[1], currentAngles[2], currentAngles[3]};
     }
-    lastAngles.angle1 = angle1; 
-  }
-  delay(100);
-  
-  if(angle2 != -1) { 
-    int currentAngle = lastAngles.angle2;  // Use tracked value, not servo.read()
-    if (currentAngle < angle2) {
-      for (int angle = currentAngle; angle <= angle2; angle++) {
-        elbow.write(angle);
-        delay(speed);
-      }
-    } else if (currentAngle > angle2) {
-      for (int angle = currentAngle; angle >= angle2; angle--) {
-        elbow.write(angle);
-        delay(speed);
-      }
-    }
-    lastAngles.angle2 = angle2; 
-  }
-  delay(100);
-  
-  if(angle3 != -1) { 
-    int currentAngle = lastAngles.angle3;  // Use tracked value, not servo.read()
-    if (currentAngle < angle3) {
-      for (int angle = currentAngle; angle <= angle3; angle++) {
-        wrist.write(angle);
-        delay(speed);
-      }
-    } else if (currentAngle > angle3) {
-      for (int angle = currentAngle; angle >= angle3; angle--) {
-        wrist.write(angle);
-        delay(speed);
-      }
-    }
-    lastAngles.angle3 = angle3; 
-  }
-  delay(100);
-  
-  if(angle4 != -1) { 
-    int currentAngle = lastAngles.angle4;  // Use tracked value, not servo.read()
-    if (currentAngle < angle4) {
-      for (int angle = currentAngle; angle <= angle4; angle++) {
-        gripper.write(angle);
-        delay(speed);
-      }
-    } else if (currentAngle > angle4) {
-      for (int angle = currentAngle; angle >= angle4; angle--) {
-        gripper.write(angle);
-        delay(speed);
-      }
-    }
-    lastAngles.angle4 = angle4; 
-  }
-  delay(100);
-  
-  return lastAngles;
+
+    return lastAngles;
 }
 
 void reachAndGrab(){
